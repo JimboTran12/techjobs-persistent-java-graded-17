6364c990882a5dd466762d49a071b97398eed7b4
Jimmy <hungtran2003ti@gmail.com>
2024-02-22 18:01:08 -0600

    ga done

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,38 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Skill extends AbstractEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Skill extends AbstractEntity {
+    private String description;
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs;
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+    public Skill(){jobs = new ArrayList<Job>();}
+
+    public Skill(String description, ArrayList<Job> jobs) {
+        this.description = description;
+        this.jobs = jobs;
+    }
 }
